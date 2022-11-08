4b9fdd81bbc9606194512abf3d01b42e73830d7e
saikrishna446 <66399465+saikrishna446@users.noreply.github.com>
2022-11-08 10:15:12 +0530

    adding dummy file

---
+++ src/test/java/selenium/Dummy.java
@@ -0,0 +1,11 @@
+package selenium;
+
+public class Dummy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is dummy class");
+
+	}
+
+}

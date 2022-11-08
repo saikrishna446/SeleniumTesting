6e2c5bf3cbd7e05cf6be6eaf5e0d01079e1b5dc7
saikrishna446 <66399465+saikrishna446@users.noreply.github.com>
2022-11-08 10:32:54 +0530

    testiing

--- src/test/java/selenium/Dummy.java
+++ src/test/java/selenium/Dummy.java
@@ -5,7 +5,7 @@ public class Dummy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my class");
-		System.out.println("this is saikrishna file");
+		System.out.println("this is saikrishna");
 
 	}
 
